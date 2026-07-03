b58dfd096be27f093c055b51680599f2ca3a7df1
D.M.C Dissanayake <132731661+Chamindu-DM@users.noreply.github.com>
2026-07-03 10:00:36 +0530

    feat: implement dashboard layout and data visualization components with Tailwind CSS integration

---
+++ src/components/charts/Simulation.tsx
@@ -0,0 +1,7 @@
+export function Simulation(){
+    return(
+        <div className="w-full h-full bg-white rounded-2xl outline outline-1 outline-offset-[-1px] outline-sky-500/20 flex flex-col justify-start items-start overflow-hidden">
+            <p>Simulation</p>
+        </div>
+    )
+}
---
+++ src/components/charts/WaveHeight.tsx
@@ -0,0 +1,7 @@
+export function WaveHeight() {
+    return (
+        <div className="w-full h-full bg-white rounded-2xl outline outline-1 outline-offset-[-1px] outline-sky-500/20 flex flex-col justify-start items-start overflow-hidden">
+            <p>Wave Height</p>
+        </div>
+    )
+}
