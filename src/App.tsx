3bfaedb878ec39218efbd90f072d93d2408b27c8
D.M.C Dissanayake <132731661+Chamindu-DM@users.noreply.github.com>
2026-07-03 19:45:21 +0530

    fix: update dashboard layout to be responsive by switching from grid to flex column on mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <div className="w-screen h-screen bg-sky-50 flex flex-col justify-start items-start">
       <AppHeader />
       <div className='flex-1 self-stretch p-2 flex flex-col justify-start items-start gap-2'>
-        <div className='w-full h-full grid grid-cols-3 grid-rows-2 gap-2'>
+        <div className='w-full flex flex-col md:grid md:grid-cols-3 md:grid-rows-2 md:h-full gap-2'>
           <Simulation/>
           <DailyMetrics/>
           <PowerOutputChart/>
